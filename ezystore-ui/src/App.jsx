8f43d1af12ee73218fb2f705a78080d3c8711f0f
sushantpaudel77 <sushantpaudel77@gmail.com>
2025-06-23 15:33:40 +0545

    Add: dynamic pageheading and title

--- ezystore-ui/src/App.jsx
+++ ezystore-ui/src/App.jsx
@@ -1,12 +1,18 @@
+import React from "react";
 import { Footer } from "./components/footer/Footer";
 import Header from "./components/Header";
+import Home from "./components/Home";
+import Pageheading from "./components/Pageheading";
+
 
 function App() {
   return (
-    <div>
+    <>
       <Header />
+      {/* <Pageheading /> */}
+      <Home />
       <Footer />
-    </div>
+    </>
   );
 }
 
---
+++ ezystore-ui/src/components/Home.jsx
@@ -0,0 +1,12 @@
+import Pageheading from "./Pageheading";
+
+export default function Home() {
+  return (
+    <div className="home-container">
+      <Pageheading title="Explore Ezy Stickers!" >
+        Add a touch of creativity to your space with out wide range of fun and
+        unique stickers. Perfect for any occasion!
+      </Pageheading>
+    </div>
+  );
+}

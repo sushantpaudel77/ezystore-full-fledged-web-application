11dea2adcf2fe2d386ec01cdb9dda245c7614dc5
Sushant Paudel <sushantpaudel77@gmail.com>
2025-06-24 18:07:28 +0545

    Delete ezystore-ui directory

--- ezystore-ui/src/components/BootstrapButton.jsx
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export const BootstrapButton = ({ text, type }) => {
-  return <button className={`btn btn-${type}`}>{text}</button>;
-};
--- ezystore-ui/src/components/PageTitle.jsx
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export default function PageTitle({ title }) {
-  return <h1 className="page-title">{title}</h1>;
-}

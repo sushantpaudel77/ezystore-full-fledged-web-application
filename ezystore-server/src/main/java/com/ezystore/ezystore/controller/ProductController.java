b2899b24f4f01f928a3d48453c63c07baed18ae7
sushantpaudel77 <sushantpaudel77@gmail.com>
2025-06-24 19:39:57 +0545

    ADD: data.sql and schema sql

--- ezystore-server/src/main/java/com/ezystore/ezystore/controller/ProductController.java
+++ ezystore-server/src/main/java/com/ezystore/ezystore/controller/ProductController.java
@@ -1,10 +1,13 @@
 package com.ezystore.ezystore.controller;
 
+import com.ezystore.ezystore.dto.ProductDto;
 import com.ezystore.ezystore.service.ProductService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(path = "api/v1/products")
@@ -13,8 +16,8 @@ public class ProductController {
 
     private final ProductService productService;
 
-    public ResponseEntity<ProductDto> getProducts() {
+    public ResponseEntity<List<ProductDto>> getProducts() {
         var productsList = productService.getProducts();
-        return ResponseEntity.of(productsList);
+        return ResponseEntity.ok(productsList);
     }
 }

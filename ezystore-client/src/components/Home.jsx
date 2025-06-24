64ef83583381fc7cea5ce57847bc2362690dc3be
sushantpaudel77 <sushantpaudel77@gmail.com>
2025-06-24 22:32:22 +0545

    ADD: hooks for backend call

--- ezystore-client/src/components/Home.jsx
+++ ezystore-client/src/components/Home.jsx
@@ -1,13 +1,26 @@
-import products from "../data/products";
 import PageHeading from "./PageHeading";
 import ProductListings from "./ProductListings";
+import apiClient from "../api/apiClient";
+import { useState, useEffect } from "react";
 
 export default function Home() {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    fetchProducts();
+  }, []);
+
+  const fetchProducts = async () => {
+    const response = await apiClient.get("/products");
+    setProducts(response.data); // Update products state with fetched data
+  };
+
   return (
     <>
       <div className="max-w-[1152px] mx-auto px-6 py-8">
         <PageHeading title="Explore Eazy Stickers!">
-          Add a touch of creativity to your space with our wide range of fun and <br />
+          Add a touch of creativity to your space with our wide range of fun and{" "}
+          <br />
           unique stickers. Perfect for any occasion!
         </PageHeading>
         <ProductListings products={products} />

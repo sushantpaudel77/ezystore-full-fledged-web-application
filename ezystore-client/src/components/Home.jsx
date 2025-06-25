fe31f44a09bd45a1029979ab197eeabb0d0c93b5
sushantpaudel77 <sushantpaudel77@gmail.com>
2025-06-25 23:56:07 +0545

    Add: loader function

--- ezystore-client/src/components/Home.jsx
+++ ezystore-client/src/components/Home.jsx
@@ -2,50 +2,11 @@ import PageHeading from "./PageHeading";
 import ProductListings from "./ProductListings";
 import apiClient from "../api/apiClient";
 import { useState, useEffect } from "react";
+import React from "react";
+import { useLoaderData } from "react-router-dom";
 
-// Hooks
-export default function Home() {
-  const [products, setProducts] = useState([]);
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(null);
-
-  // Run once when the component mounts
-  // Mounting is the process of creating and adding the component into DOM
-  useEffect(() => {
-    fetchProducts();
-  }, []);
-
-  const fetchProducts = async () => {
-    try {
-      setLoading(true);
-      const response = await apiClient.get("/products"); // Axios GET Request
-      setProducts(response.data); // Update products state with fetched data
-    } catch (error) {
-      setError(
-        error.response?.data?.message ||
-          "Failed to fetch products. Please try again."
-      ); // Extract error message if available
-    } finally {
-      setLoading(false);
-    }
-  };
-
-  if (loading) {
-    return (
-      <div className="flex items-center justify-center min-h-screen">
-        <span className="text-xl font-semibold">Loading products...</span>
-      </div>
-    );
-  }
-
-  if (error) {
-    return (
-      <div className="flex items-center justify-center min-h-screen">
-        <span className="text-xl text-red-500">Error: {error}</span>
-      </div>
-    );
-  }
-
+const Home = () => {
+  const products = useLoaderData();
   return (
     <div className="max-w-[1152px] mx-auto px-6 py-8">
       <PageHeading title="Explore Eazy Stickers!">
@@ -56,4 +17,13 @@ export default function Home() {
       <ProductListings products={products} />
     </div>
   );
+};
+
+export default Home;
+
+export async function productsLoader() {
+  try {
+    const response = await apiClient.get("/products");
+    return response.data;
+  } catch (error) {}
 }

5ecef461de6fc5146af9ed0b292ddfefad8c470b
sushantpaudel77 <sushantpaudel77@gmail.com>
2025-06-26 00:00:57 +0545

    Add: Contact components

--- ezystore-client/src/components/Header.jsx
+++ ezystore-client/src/components/Header.jsx
@@ -41,7 +41,7 @@ export default function Header() {
       <div className="flex items-center justify-between mx-auto max-w-[1152px] px-6 py-4">
         <Link to="/" className={navLinkClass}>
           <FontAwesomeIcon icon={faTags} className="h-8 w-8" />
-          <span className="font-bold">Eazy Stickers</span>
+          <span className="font-bold">Ezy Stickers</span>
         </Link>
         <nav className="flex items-center py-2 z-10">
           <button
